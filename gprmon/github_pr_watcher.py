6906a8156a58553c58f75a38b118e9b4005dd711
dcarrillo <daniel.carrillo@gmail.com>
2019-11-30 18:20:07 +0100

    Make http requests using concurrency with aiohttp

--- gprmon/github_pr_watcher.py
+++ gprmon/github_pr_watcher.py
@@ -1,16 +1,49 @@
+import asyncio
+import json
 import logging
 import threading
 import webbrowser
 from time import sleep
-from typing import Dict
+from typing import Dict, List
 
+import aiohttp
 import pystray
-import requests
 
 from gprmon.icon import Icon
-from gprmon.github_repo import GithubRepo
 
 logger = logging.getLogger('gprmon')
+
+GITHUB_URL = 'https://api.github.com'
+API_PATH = '/api/v3'
+CONN_TIMEOUT = 5
+MAX_CONNECTIONS = 4
+
+
+async def _fetch_url(session: aiohttp.ClientSession, url: str) -> str:
+    try:
+        logger.info(f'Requesting asynchronously: {url}')
+        async with session.get(url, allow_redirects=False) as response:
+            if response.status != 200:
+                logger.error(f'Error requesting {url} status code: {response.status}')
+                return None
+
+            return await response.text()
+    except aiohttp.ClientError as e:
+        raise(e)
+
+
+async def _fetch_all_urls(urls: List, headers: Dict) -> List[str]:
+    connector = aiohttp.TCPConnector(limit=MAX_CONNECTIONS)
+
+    try:
+        async with aiohttp.ClientSession(connector=connector,
+                                         headers=headers,
+                                         conn_timeout=CONN_TIMEOUT) as session:
+            text_responses = await asyncio.gather(*[_fetch_url(session, url) for url in urls])
+
+            return text_responses
+    except aiohttp.ClientError as e:
+        logger.error(e)
 
 
 class GithubPrWatcher(object):
@@ -26,10 +59,11 @@ class GithubPrWatcher(object):
             self.always_visible = False
 
         self.org = conf['organization']
-        self.url = conf['url']
+        self.url = f"{conf['url']}{API_PATH}"
         self.repos = conf['repos']
         self.match = conf['match']
-        self.token = conf['token']
+        self.headers = {'Authorization': f"token {conf['token']}",
+                        'Accept': f'application/vnd.github.{API_PATH.split("/")[-1]}+json'}
         self.icon = icon
         self.first_time = True
 
@@ -46,22 +80,19 @@ class GithubPrWatcher(object):
             pull_requests = []
             items = []
 
-            for repo_name in self.repos:
-                try:
-                    repo = GithubRepo(self.org, repo_name, self.token, self.url)
-                    pull_requests = repo.get_prs_by_reviewer(self.match)
-                except (requests.RequestException, ValueError) as e:
-                    logger.error(e)
-                    continue
-                except Exception as e:
-                    logger.error(f'Unhandled exception {e}')
-                    continue
+            urls = [f'{self.url}/repos/{self.org}/{repo_name}/pulls'
+                    for repo_name in self.repos]
 
-                if pull_requests:
-                    for pr in pull_requests:
-                        url = f'{self.url}/{self.org}/{repo_name}/pull/{str(pr)}'
-                        logger.info(f'PR found: {url}')
-                        items.append(pystray.MenuItem(url, lambda l: self._open_browser(url)))
+            try:
+                pull_requests = [pr for pr in asyncio.run(_fetch_all_urls(urls, self.headers)) if pr]
+            except TypeError:
+                pass
+            except Exception as e:
+                logger.error(f'Unhandled exception: f{e}')
+
+            for prs in pull_requests:
+                for pr_url in self._get_prs_by_reviewer(prs):
+                    items.append(pystray.MenuItem(pr_url, lambda l: self._open_browser(pr_url)))
 
             if not items and not self.always_visible:
                 self.icon.deactivate()
@@ -79,6 +110,17 @@ class GithubPrWatcher(object):
             self.icon.build_menu(pystray.Menu(*items))
 
             sleep(self.interval)
+
+    def _get_prs_by_reviewer(self, pull_requests: List[str]) -> List[str]:
+        matchs = []
+
+        for pr in json.loads(pull_requests):
+            for reviewer in pr['requested_reviewers']:
+                if reviewer['login'] == self.match:
+                    logger.info(f"{reviewer['login']} has a pending pull request: {pr['html_url']}")
+                    matchs.append(pr['html_url'])
+
+        return matchs
 
     def _open_browser(self, url: str):
         logger.info(f'Opening {url} using {webbrowser.get().basename}')
